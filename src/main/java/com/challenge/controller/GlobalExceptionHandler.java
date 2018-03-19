b19aff723b5fca0c226e627420f696300a8b196d
jawadkakar <jawadkakar@gmail.com>
2018-03-19 15:38:59 -0400

    init version

---
+++ src/main/java/com/challenge/controller/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.challenge.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ * This is a Exception handler class
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MultipartException.class)
+    public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
+        return "redirect:/uploadStatus";
+
+    }
+
+}
